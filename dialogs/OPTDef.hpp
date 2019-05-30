df13c9f17ea3bcd2de31c5f0d0c96c0c099eabd1
WGP_Senshi <6715079+WGPSenshi@users.noreply.github.com>
2019-05-30 10:26:50 +0200

    Changed defines to match naming convention

--- dialogs/OPTDef.hpp
+++ dialogs/OPTDef.hpp
@@ -1,4 +1,4 @@
-#define OPT_ColorBack		{0.1,0.1,0.1,1}
-#define OPT_ColorBackList	{0.2,0.2,0.2,1}
-#define OPT_ColorTextList	{0.2,0.2,0.2,1}
-#define OPT_ColorMapBack  	{0.506,0.569,0.675,1}
+#define COLORBACK		{0.1,0.1,0.1,1}
+#define COLORLISTBACK	{0.2,0.2,0.2,1}
+#define COLORLISTTEXT	{0.2,0.2,0.2,1}
+#define COLORMAPBACK  	{0.506,0.569,0.675,1}
