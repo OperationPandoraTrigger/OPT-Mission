98551779fbc1505fc79e73364ce9f15cfb9dbf92
GNCLord-MDB <lord.mdb@gmx.net>
2019-07-22 14:22:08 +0200

    OPT Mission Version 1.4.15.6.carrier#1.15

---
+++ OPT_1_4_15_6_carrier#.Altis/waffenwechsel/xeh_prep.hpp
@@ -0,0 +1,12 @@
+
+PREP(auslesenmagazine);
+PREP(bewaffnen);
+PREP(cbatext);
+PREP(entwaffnen);
+PREP(geldVorhandeneBewaffnung);
+PREP(initCBASettings);
+PREP(onLoadDialog);
+PREP(openDialog);
+PREP(postInit);
+PREP(waffenconfig);
+PREP(waffenMagazinFilter);
