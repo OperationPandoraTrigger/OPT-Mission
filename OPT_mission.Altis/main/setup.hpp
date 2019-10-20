b100bf6cfad9d07b8746dbb474f6319f1ba76708
Oppa-Schneutzel <54214445+oppa-schneutzel@users.noreply.github.com>
2019-10-20 19:07:14 +0200

    Schlacht#8 Titel angepasst

--- OPT_mission.Altis/main/setup.hpp
+++ OPT_mission.Altis/main/setup.hpp
@@ -9,7 +9,7 @@
 * wird in descprition.ext verwendet
 */
 // hier den Missionsnamen festlegen
-#define __MISSION_NAME__ "Schmiedegesellen Bia u Kratos"
+#define __MISSION_NAME__ "Der ruhmreiche Hinkfuß"
 // Versionsnummer eintragen (nur die des Base Templates!!!)
 #define __VERSION__ v MAJOR.MINOR.BUILD
 // made by
