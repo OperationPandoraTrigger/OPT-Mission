5f2e2cdb7250144477f72bc808356f4ca2d482fc
Oppa-Schneutzel <54214445+oppa-schneutzel@users.noreply.github.com>
2019-11-09 18:26:55 +0100

    Schlachtbezeichnung für Schlacht # 10 angepasst.

--- OPT_mission.Altis/main/setup.hpp
+++ OPT_mission.Altis/main/setup.hpp
@@ -9,7 +9,7 @@
 * wird in descprition.ext verwendet
 */
 // hier den Missionsnamen festlegen
-#define __MISSION_NAME__ "Bronzeriese Talos"
+#define __MISSION_NAME__ "Schild des Aeneas"
 // Versionsnummer eintragen (nur die des Base Templates!!!)
 #define __VERSION__ v MAJOR.MINOR.BUILD
 // made by
