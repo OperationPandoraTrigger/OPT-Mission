f5825d9e48fc254ca24569f155a9c41599468bef
formtapez <github.com@posthub.de>
2020-04-06 21:03:00 +0200

    dialog redesign

--- OPT_mission.Tanoa/beam/config/addbeamdialog.hpp
+++ OPT_mission.Tanoa/beam/config/addbeamdialog.hpp
@@ -20,54 +20,71 @@ class GVAR(dlg_addbeam)
         class IGUIBack_2213: IGUIBack
         {
             idc = 2213;
-            x = 11 * GUI_GRID_W + GUI_GRID_X;
+            x = 4.5 * GUI_GRID_W + GUI_GRID_X;
             y = 0 * GUI_GRID_H + GUI_GRID_Y;
-            w = 18.5 * GUI_GRID_W;
-            h = 9 * GUI_GRID_H;
+            w = 31.5 * GUI_GRID_W;
+            h = 15 * GUI_GRID_H;
             colorBackground[] = {0,0,0,0.6};
         };
         class RscFrame_1813: RscFrame
         {
             idc = 1813;
-            x = 11 * GUI_GRID_W + GUI_GRID_X;
+            x = 4.5 * GUI_GRID_W + GUI_GRID_X;
             y = 0 * GUI_GRID_H + GUI_GRID_Y;
-            w = 18.5 * GUI_GRID_W;
-            h = 9 * GUI_GRID_H;
+            w = 31.5 * GUI_GRID_W;
+            h = 15 * GUI_GRID_H;
             sizeEx = 1 * GUI_GRID_H;
         };
     };
     
     class controls
 	{
-        class FLAG_RscText_1013: RscText
+        class FLAG_RscText_1013: RscStructuredText
         {
             idc = 1013;
-            text = "Koordinaten der Beampunkte:"; //--- ToDo: Localize;
-            x = 11.5 * GUI_GRID_W + GUI_GRID_X;
+            style = ST_MULTI;
+            colorShadow[] = {0,0,0,0.5};
+            font = DEFAULTFONT;
+            colorText[] = {1,1,1,1};
+            colorBackground[] = {0,0,0,0};
+            text = "Aktuelle Liste aller Beampunkte: (editierbar)<br/>(Einfügen mit STRG+V. Neue Zeile mit SHIFT+ENTER.)<br/>Format: [[Pos_X, 0, Pos_Y], Name, Level]<br/>Level 1: Inf, 2: Inf+KFZ, 3: Alles, 4: Nach Missionsstart";
+            x = 6 * GUI_GRID_W + GUI_GRID_X;
             y = 0.5 * GUI_GRID_H + GUI_GRID_Y;
-            w = 17 * GUI_GRID_W;
-            h = 1.5 * GUI_GRID_H;
+            w = 30 * GUI_GRID_W;
+            h = 5 * GUI_GRID_H;
         };
         class FLAG_RscEdit_1413: RscEdit
         {
             idc = 1413;
             style = ST_MULTI;
-            x = 12 * GUI_GRID_W + GUI_GRID_X;
-            y = 2 * GUI_GRID_H + GUI_GRID_Y;
-            w = 16 * GUI_GRID_W;
-            h = 6 * GUI_GRID_H;
+            x = 6 * GUI_GRID_W + GUI_GRID_X;
+            y = 5.5 * GUI_GRID_H + GUI_GRID_Y;
+            w = 28.5 * GUI_GRID_W;
+            h = 8.5 * GUI_GRID_H;
         };       
         class RscButton_1613: RscButton
         {
             idc = 1613;
             style = ST_CENTER + ST_VCENTER;
             text = "Übernehmen"; //--- ToDo: Localize;
-            x = 16.5 * GUI_GRID_W + GUI_GRID_X;
-            y = 9.5 * GUI_GRID_H + GUI_GRID_Y;
+            x = 5.5 * GUI_GRID_W + GUI_GRID_X;
+            y = 15.5 * GUI_GRID_H + GUI_GRID_Y;
             w = 8 * GUI_GRID_W;
             h = 1.5 * GUI_GRID_H;
             sizeEx = (0.023 / (getResolution select 5));
             action = QUOTE([] call FUNC(addBeam););
+        };
+        class RscButton_1713: RscButton
+        {
+            idc = 1713;
+            style = ST_CENTER + ST_VCENTER;
+            text = "Schließen"; //--- ToDo: Localize;
+            x = 27.5 * GUI_GRID_W + GUI_GRID_X;
+            y = 15.5 * GUI_GRID_H + GUI_GRID_Y;
+            w = 8 * GUI_GRID_W;
+            h = 1.5 * GUI_GRID_H;
+            sizeEx = (0.023 / (getResolution select 5));
+            action = "closeDialog 0";
         };
     };
 };
--- OPT_mission.Tanoa/training/config/myposdialog.hpp
+++ OPT_mission.Tanoa/training/config/myposdialog.hpp
@@ -20,42 +20,47 @@ class GVAR(dlg_mypos)
         class IGUIBack_2212: IGUIBack
         {
             idc = 2212;
-            x = 11 * GUI_GRID_W + GUI_GRID_X;
+            x = 4.5 * GUI_GRID_W + GUI_GRID_X;
             y = 0 * GUI_GRID_H + GUI_GRID_Y;
-            w = 18.5 * GUI_GRID_W;
-            h = 9 * GUI_GRID_H;
+            w = 31.5 * GUI_GRID_W;
+            h = 11 * GUI_GRID_H;
             colorBackground[] = {0,0,0,0.6};
         };
         class RscFrame_1812: RscFrame
         {
             idc = 1812;
-            x = 11 * GUI_GRID_W + GUI_GRID_X;
+            x = 4.5 * GUI_GRID_W + GUI_GRID_X;
             y = 0 * GUI_GRID_H + GUI_GRID_Y;
-            w = 18.5 * GUI_GRID_W;
-            h = 9 * GUI_GRID_H;
+            w = 31.5 * GUI_GRID_W;
+            h = 11 * GUI_GRID_H;
             sizeEx = 1 * GUI_GRID_H;
         };
     };
     
     class controls
 	{
-        class FLAG_RscText_1012: RscText
+        class FLAG_RscText_1012: RscStructuredText
         {
             idc = 1012;
-            text = "Koordinaten der eigenen Position:"; //--- ToDo: Localize;
-            x = 11.5 * GUI_GRID_W + GUI_GRID_X;
+            style = ST_MULTI;
+            colorShadow[] = {0,0,0,0.5};
+            font = DEFAULTFONT;
+            colorText[] = {1,1,1,1};
+            colorBackground[] = {0,0,0,0};
+            text = "Koordinaten der eigenen Position:<br/>Zur Vorlage bei HL oder PL als Vorschlag für einen Beampunkt.<br/><t underline='1'>Immer auf ausreichend viel Platz im Umfeld achten!</t><br/>(Ortsname kann geändert werden. Herauskopierbar mit STRG+C.)";
+            x = 6 * GUI_GRID_W + GUI_GRID_X;
             y = 0.5 * GUI_GRID_H + GUI_GRID_Y;
-            w = 17 * GUI_GRID_W;
-            h = 1.5 * GUI_GRID_H;
+            w = 30 * GUI_GRID_W;
+            h = 5 * GUI_GRID_H;
         };
         class FLAG_RscEdit_1412: RscEdit
         {
             idc = 1412;
             style = ST_MULTI;
-            x = 12 * GUI_GRID_W + GUI_GRID_X;
-            y = 2 * GUI_GRID_H + GUI_GRID_Y;
-            w = 16 * GUI_GRID_W;
-            h = 6 * GUI_GRID_H;
+            x = 6 * GUI_GRID_W + GUI_GRID_X;
+            y = 5.5 * GUI_GRID_H + GUI_GRID_Y;
+            w = 28.5 * GUI_GRID_W;
+            h = 4.5 * GUI_GRID_H;
         };       
         class RscButton_1612: RscButton
         {
@@ -63,7 +68,7 @@ class GVAR(dlg_mypos)
             style = ST_CENTER + ST_VCENTER;
             text = "Schließen"; //--- ToDo: Localize;
             x = 16.5 * GUI_GRID_W + GUI_GRID_X;
-            y = 9.5 * GUI_GRID_H + GUI_GRID_Y;
+            y = 11.5 * GUI_GRID_H + GUI_GRID_Y;
             w = 8 * GUI_GRID_W;
             h = 1.5 * GUI_GRID_H;
             sizeEx = (0.023 / (getResolution select 5));
